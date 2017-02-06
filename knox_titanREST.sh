7cdbd2fea56501d0759be158e20036eb7f8dea13
Wancy <flymydream2014@gmail.com>
2017-02-06 10:34:14 -0800

    Update knox_titanREST.sh

--- knox_titanREST.sh
+++ knox_titanREST.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
 
-CURL="curl -Hcontent-type:application/json -k -u guest:guest-password"
-KNOX="https://localhost:8443/gateway/default/titan"
-
-$CURL "$KNOX?gremlin=100-1"
-echo ""
-$CURL "$KNOX/%20-d%20'{\"gremlin\":%20\"100-1\"}'"
-echo ""
-$CURL "$KNOX/graphs%20-d%20'{\"gremlin\":%20\"100-1\"}'"
-echo ""
-$CURL "$KNOX/graphs?gremlin=100-1"
